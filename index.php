e27a3363832c2ee2c73b496f12845eb90075951d
YIHONG <akashidaiki2@gmail.com>
2023-12-29 10:27:35 +0800

    先完成首頁需要的基本資料內容

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-﻿<!DOCTYPE html
-  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -13,16 +12,17 @@
 </head>
 
 <body>
-  <div id="alerr"
-    style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
+  <div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
     <pre id="ssaa"></pre>
   </div>
   <iframe name="back" style="display:none;"></iframe>
   <div id="all">
     <div id="title">
-      00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
-    <div id="title2">
-
+      <?= date("m月d日 l") ?> | 今日瀏覽: 1 | 累積瀏覽: 36
+      <a href="index.php" style='float:right'>回首頁</a>
+    </div>
+    <div id="title2" title="健康促進網-回首頁">
+      <img src="./icon/02B01.jpg" alt="">
     </div>
     <div id="mm">
       <div class="hal" id="lef">
@@ -34,8 +34,9 @@
       </div>
       <div class="hal" id="main">
         <div>
-
-          <span style="width:18%; display:inline-block;">
+          <!-- 不要剛好占滿 100 % -->
+          <marquee style="width:78%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家互相交流、分享的園地 ! 詳見最新文章</marquee>
+          <span style="width:20%; display:inline-block;">
             <a href="?do=login">會員登入</a>
           </span>
           <div class="">
