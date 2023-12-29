be27f8ee423334439c545db48b898df810abf4bf
YIHONG <akashidaiki2@gmail.com>
2023-12-29 11:34:10 +0800

    建立會員登入畫面

---
+++ front/login.php
@@ -0,0 +1,19 @@
+<fieldset>
+  <legend>會員登入</legend>
+  <table>
+    <tr>
+      <td>帳號</td>
+      <td><input type="text" name="" id=""></td>
+    </tr>
+    <tr>
+      <td>密碼</td>
+      <td><input type="password" name="" id=""></td>
+    </tr>
+    <tr>
+      <td><input type="submit" value=""><input type="reset" value=""></td>
+      <td>
+        <a href="?do=forget">忘記密碼</a> | <a href="?do=register">尚未註冊</a>
+      </td>
+    </tr>
+  </table>
+</fieldset>
