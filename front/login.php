a37f251907a2b49d239b36e37eac9cfe849d8e9d
YIHONG <akashidaiki2@gmail.com>
2023-12-29 11:46:18 +0800

    建立會員註冊畫面

--- front/login.php
+++ front/login.php
@@ -2,15 +2,15 @@
   <legend>會員登入</legend>
   <table>
     <tr>
-      <td>帳號</td>
-      <td><input type="text" name="" id=""></td>
+      <td class='clo'>帳號</td>
+      <td><input type="text" name="acc" id="acc"></td>
     </tr>
     <tr>
-      <td>密碼</td>
-      <td><input type="password" name="" id=""></td>
+      <td class='clo'>密碼</td>
+      <td><input type="password" name="pw" id="pw"></td>
     </tr>
     <tr>
-      <td><input type="submit" value=""><input type="reset" value=""></td>
+      <td><input type="submit" value="登入"><input type="reset" value="清除"></td>
       <td>
         <a href="?do=forget">忘記密碼</a> | <a href="?do=register">尚未註冊</a>
       </td>
