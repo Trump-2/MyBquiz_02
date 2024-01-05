f0f46aa4b5342976ed60823767abd8992ab33b70
YIHONG <akashidaiki2@gmail.com>
2024-01-05 11:31:09 +0800

    添加註解並修正帳號無法刪除的 bug

--- back/admin.php
+++ back/admin.php
@@ -11,7 +11,7 @@
        <?php
         $rows = $User->all();
         foreach ($rows as $row) {
-          if ($row['acc'] != 'admin') { // 
+          if ($row['acc'] != 'admin') { // 將 admin 隱藏不顯示
         ?>
            <tr>
              <td><?= $row['acc'] ?></td>
@@ -26,40 +26,40 @@
         ?>
 
      </table>
-   </form>
-   <table>
-     <!-- 使用題目內建的 class -->
-     <div class="ct">
-       <input type="submit" value="確定刪除"><input type="reset" value="清空選取">
-     </div>
 
-     <h2>新增會員</h2>
-     <span style="color:red">請設定您要註冊的帳號及密碼 ( 最長12個字元 )</span>
      <table>
-       <tr>
-         <td class="clo">Step1:登入帳號</td>
-         <td><input type="text" name="acc" id="acc"></td>
-       </tr>
-       <tr>
-         <td class="clo">Step2:登入密碼</td>
-         <td><input type="password" name="pw" id="pw"></td>
-       </tr>
-       <tr>
-         <td class="clo">Step3:再次確認密碼</td>
-         <td><input type="password" name="pw2" id="pw2"></td>
-       </tr>
-       <tr>
-         <td class="clo">Step4:信箱(忘記密碼時使用)</td>
-         <td><input type="text" name="email" id="email"></td>
-       </tr>
-       <tr>
-         <td>
-           <input type="button" value="註冊" onclick="reg()">
-           <input type="reset" value="清除">
-         </td>
-         <td></td>
-       </tr>
-     </table>
+       <!-- 使用題目內建的 class -->
+       <div class="ct">
+         <input type="submit" value="確定刪除"><input type="reset" value="清空選取">
+       </div>
+   </form>
+   <h2>新增會員</h2>
+   <span style="color:red">請設定您要註冊的帳號及密碼 ( 最長12個字元 )</span>
+   <table>
+     <tr>
+       <td class="clo">Step1:登入帳號</td>
+       <td><input type="text" name="acc" id="acc"></td>
+     </tr>
+     <tr>
+       <td class="clo">Step2:登入密碼</td>
+       <td><input type="password" name="pw" id="pw"></td>
+     </tr>
+     <tr>
+       <td class="clo">Step3:再次確認密碼</td>
+       <td><input type="password" name="pw2" id="pw2"></td>
+     </tr>
+     <tr>
+       <td class="clo">Step4:信箱(忘記密碼時使用)</td>
+       <td><input type="text" name="email" id="email"></td>
+     </tr>
+     <tr>
+       <td>
+         <input type="button" value="註冊" onclick="reg()">
+         <input type="reset" value="清除">
+       </td>
+       <td></td>
+     </tr>
+   </table>
  </fieldset>
 
  <script>
