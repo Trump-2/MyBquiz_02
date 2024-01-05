b58e41c25d7a5113f072f25855001ce4f898d79c
YIHONG <akashidaiki2@gmail.com>
2024-01-05 09:40:29 +0800

    完成點下頁籤後顯示對應內容區的機制

--- front/main.php
+++ front/main.php
@@ -12,6 +12,7 @@
 
   .tags {
     display: flex;
+    /* 把頁籤那排拉回來跟內容區對齊 */
     margin-left: 1px;
     background-color: yellow;
 
@@ -19,9 +20,11 @@
 
   article section {
     border: 1px solid black;
-    min-height: 400px;
+    min-height: 480px;
     /* 讓兩條線重疊在一起 */
     margin-top: -1px;
+
+    display: none;
   }
 
   .active {
@@ -30,16 +33,24 @@
   }
 </style>
 <div class="tags">
-  <div class="tag active">健康新知</div>
-  <div class="tag">菸害防治</div>
-  <div class="tag">癌症防治</div>
-  <div class="tag">慢性病防治</div>
+  <div id="sec01" class="tag active">健康新知</div>
+  <div id="sec02" class="tag">菸害防治</div>
+  <div id="sec03" class="tag">癌症防治</div>
+  <div id="sec04" class="tag">慢性病防治</div>
 </div>
 <article>
-  <section></section>
-  <section></section>
-  <section></section>
-  <section></section>
+  <section id="section01" style="display:block"> <!-- 讓這個 section 一開始就出現 -->
+    <h2>健康新知</h2>
+  </section>
+  <section id="section02">
+    <h2>菸害防治</h2>
+  </section>
+  <section id="section03">
+    <h2>癌症防治</h2>
+  </section>
+  <section id="section04">
+    <h2>慢性病防治</h2>
+  </section>
 </article>
 
 <script>
@@ -48,6 +59,12 @@
       $(".tag").removeClass("active");
       // $(this) 代表被點擊的那個頁籤
       $(this).addClass("active");
+      let id = $(this).attr('id');
+      // 下面這段是點下各頁籤顯示對應的內容區
+      let newID = id.replace('sec', 'section');
+      console.log(newID);
+      $("section").hide();
+      $(`#${newID}`).show();
     })
   })
 </script>
