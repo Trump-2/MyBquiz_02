952083f0e27a76130e0eceb4ec918deb4520efb0
YIHONG <akashidaiki2@gmail.com>
2024-01-05 09:21:56 +0800

    添加註解

--- front/main.php
+++ front/main.php
@@ -5,6 +5,7 @@
     border: 1px solid black;
     /* 讓兩條線重疊在一起 */
     margin-left: -1px;
+    background-color: green;
     border-radius: 5px 5px 0 0;
     text-align: center;
   }
@@ -12,16 +13,19 @@
   .tags {
     display: flex;
     margin-left: 1px;
+    background-color: yellow;
 
   }
 
   article section {
     border: 1px solid black;
     min-height: 400px;
+    /* 讓兩條線重疊在一起 */
     margin-top: -1px;
   }
 
   .active {
+    /* 蓋過內容區的 border */
     border-bottom: 1px solid white;
   }
 </style>
@@ -42,6 +46,7 @@
   $(function() {
     $(".tag").on('click', function() {
       $(".tag").removeClass("active");
+      // $(this) 代表被點擊的那個頁籤
       $(this).addClass("active");
     })
   })
