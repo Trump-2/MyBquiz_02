9260e3059b00c18a3f79496b2b3041eed4083041
YIHONG <akashidaiki2@gmail.com>
2024-01-08 15:54:57 +0800

    修改程式的 bug

--- front/vote.php
+++ front/vote.php
@@ -1,19 +1,19 @@
 <?php
 
-$que = $Que->find($_GET['id'])
+$que = $Que->find($_GET['id']);
+
 ?>
-
 <fieldset>
-  <legend>目前位置:首頁 > 問卷調查 > <?= $que['text'] ?></legend>
-  <h3><?= $que['text'] ?></h3>
-
+  <legend>目前位置:首頁 > 問卷調查 > <?= $que['text']; ?></legend>
+  <h3><?= $que['text']; ?></h3>
 
   <form action="./api/vote.php" method="post">
     <?php
+
     $opts = $Que->all(['subject_id' => $_GET['id']]);
     foreach ($opts as $opt) {
       echo "<div>";
-      echo "<input type='radio' name='' value='{$opt['id']}'>";
+      echo "<input type='radio' name='opt' value='{$opt['id']}'>";
       echo $opt['text'];
       echo "</div>";
     }
